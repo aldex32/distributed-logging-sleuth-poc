4260e8973f69c066ae1fe88ee3821dc00b930b86
Aldo Sinanaj <asinanaj@schubergphilis.com>
2017-08-25 18:25:27 +0200

    [service-4] New service with JAX-RS CXF

---
+++ service-4/src/main/java/com/schubergphilis/Service4Configuration.java
@@ -0,0 +1,33 @@
+package com.schubergphilis;
+
+import com.schubergphilis.rest.HelloService;
+import org.apache.cxf.Bus;
+import org.apache.cxf.endpoint.Server;
+import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Service4Configuration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Service4Configuration.class, args);
+    }
+
+    @Autowired
+    private Bus bus;
+
+    @Bean
+    public Server rsServer() {
+        final JAXRSServerFactoryBean serverFactory = new JAXRSServerFactoryBean();
+        serverFactory.setBus(bus);
+        serverFactory.setAddress("/");
+        serverFactory.setServiceBeans(Arrays.asList(new HelloService()));
+
+        return serverFactory.create();
+    }
+}
