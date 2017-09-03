3f6f96177c0341d69350a3e7eda653c346c770de
Aldo Sinanaj <asinanaj@schubergphilis.com>
2017-09-03 16:46:18 +0200

    [service-3] Created configuration class for Sleuth-CXF

--- service-3/src/main/java/com/schubergphilis/Service3Configuration.java
+++ service-3/src/main/java/com/schubergphilis/Service3Configuration.java
@@ -5,14 +5,18 @@ import com.schubergphilis.rest.interceptor.SleuthInInterceptor;
 import com.schubergphilis.rest.interceptor.SleuthOutInterceptor;
 import org.apache.cxf.jaxrs.client.ClientConfiguration;
 import org.apache.cxf.jaxrs.client.WebClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.sleuth.Sampler;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.Filter;
+import java.util.Collection;
 
 @SpringBootApplication
 public class Service3Configuration {
@@ -27,35 +31,48 @@ public class Service3Configuration {
     }
 
     @Bean
-    public SleuthOutInterceptor sleuthOutInterceptor() {
-        return new SleuthOutInterceptor();
-    }
-
-    @Bean
-    public SleuthInInterceptor sleuthInInterceptor() {
-        return new SleuthInInterceptor();
-    }
-
-    @Bean
     public WebClient webClient(@Value("${service4.url:http://localhost:8085}") String service4Url) {
-        final WebClient webClient = WebClient.create(service4Url + "/sayHello");
-        final ClientConfiguration clientConfiguration = WebClient.getConfig(webClient);
-        clientConfiguration.getOutInterceptors().add(sleuthOutInterceptor());
-        clientConfiguration.getInInterceptors().add(sleuthInInterceptor());
-
-        return webClient;
+        return WebClient.create(service4Url + "/sayHello");
     }
 
-    @Bean
-    public FilterRegistrationBean sleuthFilterRegistration() {
-        final FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
-        filterRegistrationBean.setFilter(getSleuthFilter());
+    @Configuration
+    protected static class SleuthWebClientConfiguration {
 
-        return filterRegistrationBean;
-    }
+        @Autowired
+        public Collection<WebClient> webClients;
 
-    @Bean
-    public Filter getSleuthFilter() {
-        return new SleuthFilter();
+        @Bean
+        public SleuthOutInterceptor sleuthOutInterceptor() {
+            return new SleuthOutInterceptor();
+        }
+
+        @Bean
+        public SleuthInInterceptor sleuthInInterceptor() {
+            return new SleuthInInterceptor();
+        }
+
+        @PostConstruct
+        public void init() {
+            if (this.webClients != null) {
+                for (WebClient webClient : this.webClients) {
+                    ClientConfiguration clientConfiguration = WebClient.getConfig(webClient);
+                    clientConfiguration.getOutInterceptors().add(sleuthOutInterceptor());
+                    clientConfiguration.getInInterceptors().add(sleuthInInterceptor());
+                }
+            }
+        }
+
+        @Bean
+        public FilterRegistrationBean sleuthFilterRegistration() {
+            final FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+            filterRegistrationBean.setFilter(getSleuthFilter());
+
+            return filterRegistrationBean;
+        }
+
+        @Bean
+        public Filter getSleuthFilter() {
+            return new SleuthFilter();
+        }
     }
 }
