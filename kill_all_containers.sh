9810a438b876146b53bfa27dacb3b54eee0578e9
Aldo Sinanaj <asinanaj@schubergphilis.com>
2017-09-03 14:44:10 +0200

    Updated shell scripts

--- kill_all_containers.sh
+++ kill_all_containers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Killing containers"
-docker rm -f  $(docker ps -a | grep -E "(service-1|service-2|service-3|kafka|zookeeper|zipkin|splunk)" | awk '{ print $1 }')
+docker rm -f  $(docker ps -a | grep -E "(service-1|service-2|service-3|service-4|kafka|zookeeper|zipkin|splunk)" | awk '{ print $1 }')
